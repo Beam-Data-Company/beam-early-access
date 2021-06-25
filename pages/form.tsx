b5213ef3eb6ec57e1e313ee47419483e89d62aee
mingsorawit <ming_s2000@hotmail.com>
2021-06-25 14:05:00 +0700

    update code as suggested

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,66 +1,61 @@
-import React, { useState }from "react";
-import MailchimpSubscribe from "react-mailchimp-subscribe";
+import React, { useState } from 'react';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 type Props = {
   status: string | null;
   message: string | Error | null;
-  onValidated: (obj: {EMAIL: string;}) => void;
+  onValidated: (obj: { EMAIL: string }) => void;
 };
 
-const CustomForm = ({ status, message, onValidated }: Props) => {
-  
+function CustomForm(props: Props) {
   const [email, setEmail] = useState('');
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(event.target){
-      setEmail(event.target.value);
-    }
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
   }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    alert('An email was submitted') 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     email &&
-    email.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email,
-    });
+      props.onValidated({
+        EMAIL: email,
+      });
   }
 
-  console.log(email);
-  console.log(status);
-  console.log(message);
-
   return (
-    <form className="mc__form" onSubmit={(event) => handleSubmit(event)}>
-      <h3 className="mc__title">Get early access for Enterprise</h3>      
+    <form className="mc__form" onSubmit={handleSubmit}>
+      <h3 className="mc__title">Get early access for Enterprise</h3>
       <div className="mc__field-container">
-        <input type="email" value={email} placeholder="your@email.com" onChange={(event) => handleChange(event)} required/>
+        <input
+          type="email"
+          value={email}
+          placeholder="your@email.com"
+          onChange={handleChange}
+          required
+        />
       </div>
-      <input type='submit' value="Submit"/>
+      <input type="submit" value="Submit" />
     </form>
   );
-};
+}
 
-
-const MailchimpFormContainer = () => {
-
+function MailchimpFormContainer() {
   const postUrl = `https://beamdata.us18.list-manage.com/subscribe/post?u=${process.env.NEXT_PUBLIC_MAILCHIMP_U}&id=${process.env.NEXT_PUBLIC_MAILCHIMP_ID}`;
-  
+
   return (
     <div className="mc__form-container">
-      <MailchimpSubscribe 
+      <MailchimpSubscribe
         url={postUrl}
         render={({ subscribe, status, message }) => (
           <CustomForm
             status={status}
             message={message}
-            onValidated={formData => subscribe(formData)}
+            onValidated={subscribe}
           />
         )}
       />
     </div>
   );
-};
+}
 
 export default MailchimpFormContainer;
