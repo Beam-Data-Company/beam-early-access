95905b4cd5790538ae500ea66b1d8df333decd07
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2021-06-25 15:14:18 +0700

    Format file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,23 +1,7 @@
 module.exports = {
   printWidth: 80,
-  semi: true,
+  semi: false,
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
-  "editor.codeActionsOnSave": {
-    "source.fixAll.eslint": true
-  },
-  "eslint.format.enable": true,
-  "[javascript]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode"
-  },
-  "[typescriptreact]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode"
-  },
-  "[json]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode"
-  },
-  "[jsonc]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode"
-  },
-};
+}
