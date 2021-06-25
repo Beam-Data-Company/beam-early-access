16ac84deff9166c7cde66282a5b2c2cbc20292bb
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2021-06-25 15:25:38 +0700

    Convert file to TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'next', 
-    'next/core-web-vitals'
+    'next',
+    'next/core-web-vitals',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,6 +21,6 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 }
