12352ca06438debb40a6bd106b1cf69b15125725
mingsorawit <ming_s2000@hotmail.com>
2021-07-02 13:27:51 +0700

    display form after closing modal

--- components/MailchimpForm.tsx
+++ components/MailchimpForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import MailchimpSubscribe, { EmailFormFields } from 'react-mailchimp-subscribe'
 import classNames from 'classnames'
 import styles from '../components/MailchimpForm.module.css'
@@ -25,40 +25,41 @@ function CustomForm(props: Props) {
     })
   }
 
-  const renderEmailForm = () => {
+  useEffect(() => {
     if (props.status === 'success') {
-      return (
-        <div>
-          <div className={styles.alert}>Thank you!</div>
-          <ThankYouModal />
-        </div>
-      )
+      setEmail('')
     }
+  }, [props.status])
 
+  const renderEmailForm = () => {
     return (
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          placeholder="Your work email address"
-          onChange={handleChange}
-          className={classNames(
-            styles.input,
-            props.status === 'error' && styles.inputError,
-            props.status === 'sending' && styles.inputDisable
-          )}
-          disabled={props.status === 'sending'}
-          required
-        />
-        <button className={styles.button} type="submit">
-          {props.status === 'sending' ? <Loading /> : 'Get Access \u2192'}
-        </button>
-      </form>
+      <>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            placeholder="Your work email address"
+            onChange={handleChange}
+            className={classNames(
+              styles.input,
+              props.status === 'error' && styles.inputError,
+              props.status === 'sending' && styles.inputDisable
+            )}
+            disabled={props.status === 'sending'}
+            required
+          />
+          <button className={styles.button} type="submit">
+            {props.status === 'sending' ? <Loading /> : 'Get Access \u2192'}
+          </button>
+        </form>
+        {props.status === 'success' ? <ThankYouModal /> : null}
+      </>
     )
   }
 
   const checkSubscribed = new RegExp('already subscribed', 'g')
-  const returnMessage = String(props.message)
+  const returnMessage =
+    typeof props.message === 'string' ? String(props.message) : ''
   const showSubscribedError =
     props.status === 'error' && checkSubscribed.test(returnMessage)
 
@@ -76,7 +77,7 @@ function CustomForm(props: Props) {
           showSubscribedError && styles.show_error_box
         )}
       >
-        {showSubscribedError ? 'This email is already subscribed.' : ''}
+        {showSubscribedError && 'This email is already subscribed.'}
       </div>
     </div>
   )
