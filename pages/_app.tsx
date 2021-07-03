13f164cdc4101415e159df7dfe1e3d14c9bd7fd1
Kunanan <kunanan@beamdata.co>
2021-07-03 16:48:23 +0700

    Mount `Modal` to the top level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import Modal from 'react-modal';
 
 import SEO from '../next-seo.config'
+
+Modal.setAppElement('#__next');
 
 function App({ Component, pageProps }: AppProps) {
   return (
