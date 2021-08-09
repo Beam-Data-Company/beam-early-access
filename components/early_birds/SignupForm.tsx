3e1ab70981c4d4d7b3e5bdbdfa115a5e7a56916a
mingsorawit <ming_s2000@hotmail.com>
2021-08-09 13:26:23 +0700

    create SignupForm component and css

---
+++ components/early_birds/SignupForm.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import styles from './SignupForm.module.css'
+
+export default function SignupForm() {
+  return (
+    <Formik
+      initialValues={{ fullName: '', country: '', phoneNumber: '', email: '' }}
+      validationSchema={Yup.object({
+        fullName: Yup.string()
+          .max(30, 'Must be 30 characters or less')
+          .required('Required'),
+        country: Yup.string()
+          .oneOf(['Thailand', 'Singapore', 'USA'])
+          .required('Required'),
+        phoneNumber: Yup.string()
+          .max(10, 'Must be 10 characters or less')
+          .required('Required'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      <Form className={styles.form_container}>
+        <Field
+          name="fullName"
+          placeholder="Full Name"
+          className={styles.input_field}
+        />
+        <ErrorMessage name="fullName" />
+
+        <div className={styles.phone_number_container}>
+          <Field name="country" as="select" className={styles.country}>
+            <option value="Thailand">+66</option>
+            <option value="Singapore">+65</option>
+            <option value="USA">+1</option>
+          </Field>
+          <Field
+            name="phoneNumber"
+            placeholder="080 969 6462"
+            className={styles.phone_input}
+          />
+        </div>
+        <ErrorMessage name="phoneNumber" />
+        <Field
+          name="email"
+          type="email"
+          placeholder="Work Email"
+          className={styles.input_field}
+        />
+        <ErrorMessage name="email" />
+
+        <button type="submit" className={styles.submit_button}>
+          Submit
+        </button>
+      </Form>
+    </Formik>
+  )
+}
