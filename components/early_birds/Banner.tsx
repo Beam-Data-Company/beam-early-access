45a7c0d6cc3dcad48538b03c8bbfb9eb3fa26588
mingsorawit <ming_s2000@hotmail.com>
2021-08-09 13:25:55 +0700

    create Banner component and css

---
+++ components/early_birds/Banner.tsx
@@ -0,0 +1,10 @@
+import styles from './Banner.module.css'
+
+export default function Banner() {
+  return (
+    <div className={styles.container}>
+      <h1>#BeamEarlyBirds</h1>
+      <h3>Feel Free, Free of Fees!</h3>
+    </div>
+  )
+}
