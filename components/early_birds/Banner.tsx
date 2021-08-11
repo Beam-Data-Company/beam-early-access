0c16ec79a723e27597e52aa4a1b1b2f6ffbd3d65
mingsorawit <ming_s2000@hotmail.com>
2021-08-11 10:21:02 +0700

    add banner picture

--- components/early_birds/Banner.tsx
+++ components/early_birds/Banner.tsx
@@ -1,10 +1,15 @@
 import styles from './Banner.module.css'
+import Image from 'next/image'
+import bannerPicture from '../../public/earlybird/banner-picture.png'
 
 export default function Banner() {
   return (
     <div className={styles.container}>
-      <h1>#BeamEarlyBirds</h1>
-      <h3>Feel Free, Free of Fees!</h3>
+      <Image src={bannerPicture} alt="Banner Picture" priority />
+      <div className={styles.text_container}>
+        <h1>#BeamEarlyBirds</h1>
+        <h3>Feel Free, Free of Fees!</h3>
+      </div>
     </div>
   )
 }
