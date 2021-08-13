d6c3d1b919e1e9085267b65ba5c6b5156bbd18f3
mingsorawit <ming_s2000@hotmail.com>
2021-08-13 09:59:38 +0700

    responsive version

--- components/early_birds/Banner.tsx
+++ components/early_birds/Banner.tsx
@@ -5,7 +5,9 @@ import bannerPicture from '../../public/earlybird/banner-picture.png'
 export default function Banner() {
   return (
     <div className={styles.container}>
-      <Image src={bannerPicture} alt="Banner Picture" priority />
+      <div className={styles.banner_picture}>
+        <Image src={bannerPicture} alt="Banner Picture" priority />
+      </div>
       <div className={styles.text_container}>
         <h1>#BeamEarlyBirds</h1>
         <h3>Feel Free, Free of Fees!</h3>
