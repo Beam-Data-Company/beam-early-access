9dff7d67867f57c66501b5f8cb754eabf8c061b7
mingsorawit <ming_s2000@hotmail.com>
2021-08-23 19:09:46 +0700

    import assets in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,31 @@
 import styles from './Footer.module.css'
+import Image from 'next/image'
+import beamLogo from '../public/footer/beam-logo.png'
+import appStoreButton from '../public/footer/app-store-button.png'
+import googlePlayButton from '../public/footer/google-play-button.png'
 
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles.top_section}></div>
+      <div className={styles.top_section}>
+
+        <div className={styles.logo_container}>
+          <div className={styles.beam_logo}>
+            <Image src={beamLogo} alt="Beam Logo" />
+          </div>
+          <div className={styles.store_button}>
+            <Image src={appStoreButton} alt="App Store" />
+          </div>
+          <div className={styles.store_button}>
+            <Image src={googlePlayButton} alt="Google Play" />
+          </div>
+        </div>
+
+        <>
+        </>
+
+      </div>
+
       <div className={styles.bottom_section}></div>
     </div>
   )
