d56f3f2157a235851012b1bedd30a3720436a6b2
mingsorawit <ming_s2000@hotmail.com>
2021-08-23 18:18:37 +0700

    create footer component and privacypolicy page

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top_section}></div>
+      <div className={styles.bottom_section}></div>
+    </div>
+  )
+}
---
+++ pages/privacypolicy.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import styles from '../styles/privacypolicy.module.css'
+
+export default function PrivacyPolicy() {
+  return (
+    <div className={styles.main_container}>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
