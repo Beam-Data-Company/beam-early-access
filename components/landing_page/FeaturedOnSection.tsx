2af6da2e1cb8c8b54faf1d1cb70ddc916c3930ab
mingbeam <ming_s2000@hotmail.com>
2021-11-24 15:46:36 +0700

    created Featured On section

---
+++ components/landing_page/FeaturedOnSection.tsx
@@ -0,0 +1,35 @@
+import styles from './FeaturedOnSection.module.css'
+import BangkokPostLogo from '../../public/landing_page/bangkok-post-logo.png'
+import ForbesLogo from '../../public/landing_page/forbes-logo.png'
+import MediumLogo from '../../public/landing_page/medium-logo.png'
+import Image from 'next/image'
+import Text from '../Text'
+
+export default function FeaturedOnSection() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Text size={20} color="#ffffff">
+          Featured On
+        </Text>
+      </div>
+      <div className={styles.logo_container}>
+        <Image src={ForbesLogo} width={90} height={60} alt="Forbes Logo" />
+        <Image
+          src={BangkokPostLogo}
+          width={153}
+          height={28}
+          alt="Bangkok Post Logo"
+        />
+        <Image src={MediumLogo} height={45} width={180} alt="Medium Logo" />
+        <Image src={ForbesLogo} width={90} height={60} alt="Forbes Logo" />
+        <Image
+          src={BangkokPostLogo}
+          width={153}
+          height={28}
+          alt="Bangkok Post Logo"
+        />
+      </div>
+    </div>
+  )
+}
