22a1d74233181540e5b127e4e0bddd9fc9e21748
mingbeam <ming_s2000@hotmail.com>
2022-01-21 16:27:23 +0700

    update and rearrange logos

--- components/landing_page/PartnerLogoCarousel.tsx
+++ components/landing_page/PartnerLogoCarousel.tsx
@@ -3,13 +3,10 @@ import Text from '../Text'
 import ImageBox from '../ImageBox'
 import { useMediaQuery } from 'react-responsive'
 
-import NaraLogoWhite from '../../public/landing_page/partner_logo/nara-logo-white.png'
 import ChanintrLogo from '../../public/landing_page/partner_logo/chanintr-logo.png'
-import ChloeLogo from '../../public/landing_page/partner_logo/chloe-logo.png'
 import DisayaLogo from '../../public/landing_page/partner_logo/disaya-logo.png'
 import HermanMillerLogo from '../../public/landing_page/partner_logo/herman-miller-logo.png'
 import HondaLogo from '../../public/landing_page/partner_logo/honda-logo.png'
-import JimmyChooLogo from '../../public/landing_page/partner_logo/jimmy-choo-logo.png'
 import LeicaLogo from '../../public/landing_page/partner_logo/leica-logo.png'
 import MatterMakersLogo from '../../public/landing_page/partner_logo/matter-makers-logo.png'
 import MercedesBenzLogo from '../../public/landing_page/partner_logo/mercedes-benz-logo.png'
@@ -19,7 +16,8 @@ import SansiriLogo from '../../public/landing_page/partner_logo/sansiri-logo.png
 import ThaiSmileLogo from '../../public/landing_page/partner_logo/thai-smile-logo.png'
 import ToochLogo from '../../public/landing_page/partner_logo/tooch-logo.png'
 import ToyotaLogo from '../../public/landing_page/partner_logo/toyota-logo.png'
-import ValentinoLogo from '../../public/landing_page/partner_logo/valentino-logo.png'
+import SuvimolLogo from '../../public/landing_page/partner_logo/suvimol-logo.png'
+import PoemLogo from '../../public/landing_page/partner_logo/poem-logo.png'
 
 export default function PartnerLogoCarousel() {
   const isPhone = useMediaQuery({ maxWidth: 600 })
@@ -33,41 +31,37 @@ export default function PartnerLogoCarousel() {
       </div>
 
       <div className={styles.slider_track}>
-        <ImageBox src={NaraLogoWhite} alt="Nara Logo White" width={150} />
         <ImageBox src={ChanintrLogo} alt="Chanintr Logo" width={160} />
-        <ImageBox src={ChloeLogo} alt="Chloe Logo" width={120} />
-        <ImageBox src={DisayaLogo} alt="Disaya Logo" width={150} />
         <ImageBox src={HermanMillerLogo} alt="Herman Miller Logo" width={200} />
         <ImageBox src={HondaLogo} alt="Honda Logo" width={100} />
-        <ImageBox src={JimmyChooLogo} alt="Jimmy Choo Logo" width={200} />
+        <ImageBox src={DisayaLogo} alt="Disaya Logo" width={150} />
         <ImageBox src={LeicaLogo} alt="Leica Logo" width={120} />
         <ImageBox src={MatterMakersLogo} alt="Matter Makers Logo" width={200} />
+        <ImageBox src={PoemLogo} alt="Poem Logo" width={130} />
         <ImageBox src={MercedesBenzLogo} alt="Mercedes Benz Logo" width={200} />
-        <ImageBox src={PergoLogo} alt="Pergo Logo" width={150} />
-        <ImageBox src={RavipaLogo} alt="Ravipa Logo" width={150} />
+        <ImageBox src={PergoLogo} alt="Pergo Logo" width={130} />
         <ImageBox src={SansiriLogo} alt="Sansiri Logo" width={120} />
+        <ImageBox src={SuvimolLogo} alt="Suvimol Logo" width={150} />
         <ImageBox src={ThaiSmileLogo} alt="Thai Smile Logo" width={100} />
-        <ImageBox src={ToochLogo} alt="Tooch Logo" width={160} />
-        <ImageBox src={ToyotaLogo} alt="Toyota Logo" width={100} />
-        <ImageBox src={ValentinoLogo} alt="Valentino Logo" width={180} />
+        <ImageBox src={RavipaLogo} alt="Ravipa Logo" width={130} />
+        <ImageBox src={ToochLogo} alt="Tooch Logo" width={120} />
+        <ImageBox src={ToyotaLogo} alt="Toyota Logo" width={80} />
 
-        <ImageBox src={NaraLogoWhite} alt="Nara Logo White" width={150} />
         <ImageBox src={ChanintrLogo} alt="Chanintr Logo" width={160} />
-        <ImageBox src={ChloeLogo} alt="Chloe Logo" width={120} />
-        <ImageBox src={DisayaLogo} alt="Disaya Logo" width={150} />
         <ImageBox src={HermanMillerLogo} alt="Herman Miller Logo" width={200} />
         <ImageBox src={HondaLogo} alt="Honda Logo" width={100} />
-        <ImageBox src={JimmyChooLogo} alt="Jimmy Choo Logo" width={200} />
+        <ImageBox src={DisayaLogo} alt="Disaya Logo" width={150} />
         <ImageBox src={LeicaLogo} alt="Leica Logo" width={120} />
         <ImageBox src={MatterMakersLogo} alt="Matter Makers Logo" width={200} />
+        <ImageBox src={PoemLogo} alt="Poem Logo" width={130} />
         <ImageBox src={MercedesBenzLogo} alt="Mercedes Benz Logo" width={200} />
-        <ImageBox src={PergoLogo} alt="Pergo Logo" width={150} />
-        <ImageBox src={RavipaLogo} alt="Ravipa Logo" width={150} />
+        <ImageBox src={PergoLogo} alt="Pergo Logo" width={130} />
         <ImageBox src={SansiriLogo} alt="Sansiri Logo" width={120} />
+        <ImageBox src={SuvimolLogo} alt="Suvimol Logo" width={150} />
         <ImageBox src={ThaiSmileLogo} alt="Thai Smile Logo" width={100} />
-        <ImageBox src={ToochLogo} alt="Tooch Logo" width={160} />
-        <ImageBox src={ToyotaLogo} alt="Toyota Logo" width={100} />
-        <ImageBox src={ValentinoLogo} alt="Valentino Logo" width={180} />
+        <ImageBox src={RavipaLogo} alt="Ravipa Logo" width={130} />
+        <ImageBox src={ToochLogo} alt="Tooch Logo" width={120} />
+        <ImageBox src={ToyotaLogo} alt="Toyota Logo" width={80} />
       </div>
     </div>
   )
