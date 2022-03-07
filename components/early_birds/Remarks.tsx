c2e282b5cb8c6501fae0d6f5ee7050a470af2864
mingbeam <ming_s2000@hotmail.com>
2022-03-07 13:40:45 +0700

    update link style

--- components/early_birds/Remarks.tsx
+++ components/early_birds/Remarks.tsx
@@ -8,11 +8,11 @@ export default function Remarks() {
       <Text size={12} color="#535353" weight={600}>
         Remarks: By registering this campaign, you agree to Beam&apos;s{' '}
         <Link href="https://www.beamcheckout.com/privacy" passHref>
-          <a className="remark_link">Privacy Policy</a>
+          <a className={styles.remark_link}>Privacy Policy</a>
         </Link>{' '}
         and{' '}
         <Link href="https://www.beamcheckout.com/tncs" passHref>
-          <a className="remark_link">Terms and Conditions</a>
+          <a className={styles.remark_link}>Terms and Conditions</a>
         </Link>
         . We reserved the rights to refuse providing offers and/or promotions at
         any time, either for particular individuals or organizations at our
