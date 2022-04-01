85d6a4909223abf911b3e3168f211030138d8f36
mingbeam <ming_s2000@hotmail.com>
2022-04-02 01:28:11 +0700

    add props in Sidebar, make it sticky in faq page

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,10 +2,13 @@ import styles from './SideBar.module.css'
 import Text from './Text'
 import Spacer from './Spacer'
 import { useMediaQuery } from 'react-responsive'
+import classNames from 'classnames'
 
 type Props = {
   contentTitle: string
   contentList: any[]
+  isFaq?: boolean
+  isThai?: boolean
 }
 
 export function generateAnchorID(title: string) {
@@ -17,16 +20,26 @@ export default function SideBar(props: Props) {
   const isPhonePortrait = useMediaQuery({ maxWidth: 450 })
 
   return (
-    <aside className={styles.container}>
-      <Text size={18} weight={600}>
+    <aside
+      className={classNames(styles.container, props.isFaq && styles.sticky)}
+    >
+      <Text
+        size={18}
+        weight={600}
+        family={props.isThai ? 'IBM Plex Sans Thai' : 'Assistant'}
+      >
         {props.contentTitle}
       </Text>
-      <Spacer height={6} />
+      <Spacer height={props.isFaq ? 24 : 6} />
       <ul className={styles.content_list}>
         {props.contentList.map((child) => (
           <li key={child.title}>
             <a href={`#${generateAnchorID(child.title)}`}>
-              <Text size={14} weight={400}>
+              <Text
+                size={14}
+                weight={400}
+                family={props.isThai ? 'IBM Plex Sans Thai' : 'Assistant'}
+              >
                 {child.title}
               </Text>
             </a>
